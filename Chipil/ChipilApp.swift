1ff1ed5a9386b2f516c16cb0b193c470dd6b6358
OscarValdes1997 <313227743@pcpuma.acatlan.unam.mx>
2023-04-24 10:44:12 -0600

    Initial Commit

---
+++ Chipil/ChipilApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChipilApp.swift
+//  Chipil
+//
+//  Created by Oscar Roberto Valdes Rodriguez on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ChipilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
